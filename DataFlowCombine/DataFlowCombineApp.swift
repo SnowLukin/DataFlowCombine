39dfd7b47dcd4c277eda8c810f3ba67d484dcbf8
Snow Lukin <everyoursden@gmail.com>
2022-02-23 13:30:05 +0300

    Initial Commit

---
+++ DataFlowCombine/DataFlowCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataFlowCombineApp.swift
+//  DataFlowCombine
+//
+//  Created by Snow Lukin on 23.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct DataFlowCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
